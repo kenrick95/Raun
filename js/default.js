07a93075ad71d23c6ed4354b994c0f2557a75117
kenrick95 <kenrick95@gmail.com>
2013-12-12 13:25:56 +0800

    bugfix on digit separator

--- js/default.js
+++ js/default.js
@@ -31,9 +31,9 @@ $(document).ready(function () {
 	// http://www.mredkj.com/javascript/numberFormat.html
 	{
 		nStr += '';
-		x = nStr.split(locale_obj['separator_decimals']);
+		x = nStr.split('.');
 		x1 = x[0];
-		x2 = x.length > 1 ? ',' + x[1] : '';
+		x2 = x.length > 1 ? locale_obj['separator_decimals'] + x[1] : '';
 		var rgx = /(\d+)(\d{3})/;
 		while (rgx.test(x1)) {
 			x1 = x1.replace(rgx, '$1' + locale_obj['separator_thousands'] + '$2');

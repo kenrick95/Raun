9026d06394ca0f42bcee261e5170d5a3a30f331a
kenrick95 <kenrick95@gmail.com>
2014-04-22 13:36:54 +0800

    use 1 request for CDN; +analytics

--- index.php
+++ index.php
@@ -62,28 +62,25 @@ ob_end_clean();
     <title>Raun</title>
     
     <!-- Bootstrap core CSS -->
-    <link href='//fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
     <link href="//cdn.jsdelivr.net/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
     
     <link href="css/style.css" rel="stylesheet">
+<?php
+    if (stripos("tools.wmflabs.org", $_SERVER["SERVER_NAME"]) !== false) {
+?>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-<!--<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-49036729-2']);
-_gaq.push(['_trackPageview']);
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-
-ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-</script>-->
+        ga('create', 'UA-49036729-2', 'wmflabs.org');
+        ga('send', 'pageview');
+    </script>
+<?php
+    }
+?>
 </head>
 <body>
     <div class="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -354,9 +351,7 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga
         </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
     <!-- JavaScript files -->
-    <script src="//cdn.jsdelivr.net/jquery/2.1.0/jquery.min.js"></script>
-    <script src="//cdn.jsdelivr.net/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdn.jsdelivr.net/nanobar/0.0.6/nanobar.min.js"></script>
+    <script src="//cdn.jsdelivr.net/g/jquery@2.1,bootstrap@3.1.1,nanobar@0.0.6"></script>
     <script src="//tools.wmflabs.org/intuition/load.php?env=standalone"></script>
     <script>
     intuition.load("raun", "<?php echo $locale; ?>");

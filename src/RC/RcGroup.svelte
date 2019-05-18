42fc1dc03bca06d6c73d63cc7cde4e1060335255
Kenrick <kenrick95@gmail.com>
2019-05-18 16:31:02 +0700

    Basic styling

--- src/RC/RcGroup.svelte
+++ src/RC/RcGroup.svelte
@@ -4,12 +4,30 @@
 </script>
 
 <style>
+  .group {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 16px 12px;
+    margin: 10px;
+    border-radius: 4px;
+    padding: 10px 15px;
+    border: 1px solid #ddd;
+    /* box-shadow: 0 0 5px #ddd; */
+  }
+  .group-title {
+    
+  grid-column: 1 / span 1;
+  grid-row:1 / span 1;
+  }
   .events {
   }
 </style>
 
 {#if eventGroup}
-  <li>
+  <li class="group">
+  <div class="group-title">
+     {eventGroup[0].title}
+  </div>
     <ul class="events">
       {#each eventGroup.reverse() as event}
         <RcEvent {event} />
